1bceb196b481bf0d93eb032e05d2524be011a9af
Tapio Valli <tapio@tapiovalli.com>
2018-11-23 20:45:56 +0200

    Before adding README

--- Src/main.c
+++ Src/main.c
@@ -141,17 +141,6 @@ int main(void)
 
 	char AT_Str[4] = "AT\r\n";
 	char AT_Rpl[4000];
-//
-//	HAL_Delay(5000);
-//
-//	while (1) {
-//		HAL_UART_Transmit(&huart2, (uint8_t *) AT_Str, 4, 1000);
-//		HAL_UART_Receive(&huart2, (uint8_t *) AT_Rpl, 4000, 10000);
-//		printf("Reply = %s", AT_Rpl);
-//		printf("\r\n ----- \r\n");
-//		HAL_Delay(5000);
-//	}
-
 
 #ifdef USART_PRINT_MSG
 	// UART_Msg_Gpio_Init();
@@ -192,6 +181,24 @@ int main(void)
 	}
 	HAL_Delay(2000);
 	printf("\rConsole Ready...\r\n");
+
+	//	AT commands to program Wifi module
+	//	AT+S.SCFG=blink_led,0
+	//	AT+S.SSIDTXT=<RobotSSID>
+	//	AT+S.SCFG=wifi_auth_type,0
+	//	AT+S.SCFG=wifi_priv_mode,2
+	//	AT+S.SCFG=wifi_wpa_psk_text,<secretkey>
+	//	AT+S.SCFG=ip_hostname,<hostname>
+	//	AT+S.SCFG=ip_apdomainname,<robot.net>
+	//	AT+S.SCFG=ip_apredirect,index.html
+	//	AT+S.WCFG
+	//
+	//	## Will erase data, DO NOT USE IF NOT INTENDED
+	//	## AT+S.FSUPDATE=i,192.168.0.51,\fs.img,,,,
+	//
+	//	## Write data to robot.fhtml table
+	//	AT+S.INPUTSSI=<strlen><CR>
+	//	|10|20|30.3|40|55|<CR>
 
   /* USER CODE END 2 */
 
